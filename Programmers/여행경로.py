aeb0a3d91c2e5a8160be5b34d69aefd2cfaaecb2
maplejh <cjh0313c@gmail.com>
2022-01-05 03:13:30 +0900

    220105 프로그래머스 여행경로

---
+++ Programmers/여행경로.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+
+def dfs(city):
+    while airport.get(city, []):
+        dfs(airport[city].pop())
+    if not airport.get(city, []):
+        answer.append(city)
+
+
+def solution(tickets):
+    global airport
+    airport = defaultdict(list)
+    global answer
+    answer = []
+    for a, b in tickets:
+        airport[a].append(b)
+    airport = dict(map(lambda x: (x[0], sorted(x[1], reverse=True)), airport.items()))
+    dfs("ICN")
+    return answer[::-1]
