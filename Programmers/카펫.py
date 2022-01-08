f6d12972cfd6db290fd9063b02932792675d3d7c
maplejh <cjh0313c@gmail.com>
2022-01-08 19:55:39 +0900

    220108 프로그래머스 카펫

---
+++ Programmers/카펫.py
@@ -0,0 +1,9 @@
+def solution(brown, yellow):
+    s = brown + yellow
+    sum_w_h = brown / 2 + 2
+    for h in range(1, int(s ** 0.5) + 1):
+        if not s % h:
+            w = s // h
+            if w + h == sum_w_h:
+                print(w, h)
+                return w, h
