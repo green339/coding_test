b793ec71b7f9a8bc5668450ed3f211e740b22342
maplejh <54804674+maplejh@users.noreply.github.com>
2022-01-10 01:04:57 +0900

    220110 프로그래머스 기능개발

--- Programmers/기능개발.py
+++ Programmers/기능개발.py
@@ -1,1 +1,15 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42586
+import math
+
+
+def solution(progresses, speeds):
+    prior = 0
+    answer = []
+    for p, s in zip(progresses, speeds):
+        temp = math.ceil((100 - p) / s)
+        if temp <= prior:
+            answer[-1] += 1
+        else:
+            answer.append(1)
+            prior = temp
+    return answer
