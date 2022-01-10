d1e11f1d7039b0f3addf09633de3f36022746bb0
maplejh <54804674+maplejh@users.noreply.github.com>
2022-01-11 00:17:56 +0900

    220111 프로그래머스 프린터

--- Programmers/프린터.py
+++ Programmers/프린터.py
@@ -1,1 +1,20 @@
-#
+# https://programmers.co.kr/learn/courses/30/lessons/42587
+from collections import deque
+
+
+def solution(priorities, location):
+    q = deque()
+    for n, p in enumerate(priorities):
+        q.append((n, p))
+    priorities.sort()
+    cnt = 0
+    temp_max = priorities.pop()
+    while True:
+        nx, px = q.popleft()
+        if px == temp_max:
+            cnt += 1
+            if nx == location:
+                return cnt
+            temp_max = priorities.pop()
+        else:
+            q.append((nx, px))
