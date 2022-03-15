660d07b7f0aa5bbaf84fc91ccb34a489ca53e7f2
maplejh <cjh0313c@gmail.com>
2022-03-15 18:42:56 +0900

    220315 프로그래머스 숫자 문자열과 영단어

---
+++ Programmers/숫자 문자열과 영단어.py
@@ -0,0 +1,9 @@
+# https://programmers.co.kr/learn/courses/30/lessons/81301
+
+def solution(s):
+    answer = s
+    numword = {"zero": "0", "one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7",
+               "eight": "8", "nine": "9"}
+    for k, v in numword.items():
+        answer = answer.replace(k, v)
+    return int(answer)
