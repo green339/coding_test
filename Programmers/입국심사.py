c624157711953fc75d387e46087f2c9dd415d053
maplejh <cjh0313c@gmail.com>
2022-04-15 23:07:37 +0900

    220415 프로그래머스 입국심사

---
+++ Programmers/입국심사.py
@@ -0,0 +1,18 @@
+# https://programmers.co.kr/learn/courses/30/lessons/43238
+def solution(n, times):
+    answer = 0
+    times.sort()
+    start = times[0]
+    end = times[-1] * n
+    while start <= end:
+        mid = (start + end) // 2
+        cnt = 0
+        for t in times:
+            cnt += mid // t
+            if cnt >= n:
+                answer = mid
+                end = mid - 1
+                break
+        else:
+            start = mid + 1
+    return answer
