7e339d1ec24102635539cb10b6b280e6551a063b
maplejh <cjh0313c@gmail.com>
2022-04-16 23:48:31 +0900

    220416 백준 13458

--- BOJ/13458.py
+++ BOJ/13458.py
@@ -8,11 +8,10 @@ if __name__ == "__main__":
     N = int(input())
     A = list(map(int, input().split()))
     B, C = map(int, input().split())
-
-    answer = 0
-    for ppl in A:
-        answer += 1
-        ppl -= B
-        if ppl > 0:
-            answer += math.ceil(ppl / C)
+    answer=0
+    for students in A:
+        if students-B<=0:
+            answer+=1
+        else:
+            answer+=1+math.ceil((students-B)/C)
     print(answer)
