cdfd3e88dcee3030030ca42d7c7ffb2424bd7b9f
maplejh <cjh0313c@gmail.com>
2022-05-03 19:32:01 +0900

    220503 프로그래머스 숫자 문자열과 영단어

--- Programmers/숫자 문자열과 영단어.py
+++ Programmers/숫자 문자열과 영단어.py
@@ -7,3 +7,11 @@ def solution(s):
     for k, v in numword.items():
         answer = answer.replace(k, v)
     return int(answer)
+
+
+def solution_v2(s):
+    number = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7",
+              "eight": "8", "nine": "9", "zero": "0"}
+    for k, v in number.items():
+        s = s.replace(k, v)
+    return int(s)
