bb061986102ce6d5bcdf8e6634a8130bcd32115f
maplejh <cjh0313c@gmail.com>
2022-05-22 23:47:00 +0900

    220522 프로그래머스 가장 큰 수

--- Programmers/가장 큰 수.py
+++ Programmers/가장 큰 수.py
@@ -6,6 +6,13 @@ def solution(numbers):
     sn = [(str(i), (str(i) * 4)[:4]) for i in numbers]
 
     sn.sort(key=lambda x: x[1], reverse=True)
-    print(sn)
     answer = ''.join([answer + i[0] for i in sn])
     return answer
+
+
+def solution_v2(numbers):
+    if max(numbers) == 0:
+        return "0"
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x * 3, reverse=True)
+    return ''.join(numbers)
