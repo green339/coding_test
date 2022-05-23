767fc8b56fda861599db62d329b192d54423d35a
maplejh <cjh0313c@gmail.com>
2022-05-24 01:23:07 +0900

    220524 프로그래머스 프린터

--- Programmers/프린터.py
+++ Programmers/프린터.py
@@ -18,3 +18,23 @@ def solution(priorities, location):
             temp_max = priorities.pop()
         else:
             q.append((nx, px))
+
+
+def solution_v2(priorities, location):
+    answer = 0
+    cur = 0
+    end = len(priorities)
+    while sum(priorities) > 0:
+        if not priorities[cur]:
+            cur = (cur + 1) % end
+            continue
+        for x in range(cur + 1, cur + end):
+            if priorities[cur] < priorities[x % end]:
+                break
+        else:
+            priorities[cur] = 0
+            answer += 1
+            if cur == location:
+                return answer
+        cur = (cur + 1) % end
+    return answer
