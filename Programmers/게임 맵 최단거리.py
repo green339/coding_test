19d9f7437bee4c8ba178ce2da2a7e2279dd37f8a
maplejh <cjh0313c@gmail.com>
2022-05-28 17:53:44 +0900

    220528 프로그래머스 게임 맵 최단거리

--- Programmers/게임 맵 최단거리.py
+++ Programmers/게임 맵 최단거리.py
@@ -20,3 +20,27 @@ def solution(maps):
                     maps[nx][ny] = 0
                     heapq.heappush(q, (c + 1, nx, ny))
     return -1
+
+
+from collections import deque
+
+d = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+
+
+def solution_v2(maps):
+    answer = 0
+    n = len(maps) - 1
+    m = len(maps[0]) - 1
+    q = deque()
+    q.append((0, 0))
+    maps[0][0] = 2
+    while q:
+        x, y = q.popleft()
+        for dx, dy in d:
+            nx = dx + x
+            ny = dy + y
+            if -1 < nx <= n and -1 < ny <= m:
+                if maps[nx][ny] == 1:
+                    maps[nx][ny] = maps[x][y] + 1
+                    q.append((nx, ny))
+    return maps[n][m] - 1 if maps[n][m] != 1 else -1
