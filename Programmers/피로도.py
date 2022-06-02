d8cc67f1711fa677f52bcbf45be1ef0d0ad01866
maplejh <cjh0313c@gmail.com>
2022-06-02 22:21:07 +0900

    220602 프로그래머스 피로도

--- Programmers/피로도.py
+++ Programmers/피로도.py
@@ -16,4 +16,23 @@ def solution(k, dungeons):
         answer = max(answer, temp)
     return answer
 
-## 백트래킹으로도 풀어보기
+## 다른 풀이
+def dfs(depth, left):
+    global answer
+    if depth > answer:
+        answer = depth
+    for i in range(len(d)):
+        if not visited[i]:
+            if left >= d[i][0]:
+                visited[i] = 1
+                dfs(depth + 1, left - d[i][1])
+                visited[i] = 0
+
+
+def solution_v2(k, dungeons):
+    global visited, d, answer
+    d = dungeons
+    visited = [0] * len(d)
+    answer = 0
+    dfs(0, k)
+    return answer
