ddf5f7fe0ba1c44fd1b865230bce511ed3069d9e
maplejh <cjh0313c@gmail.com>
2022-06-06 23:00:00 +0900

    220606 프로그래머스 영어 끝말잇기

--- Programmers/영어 끝말잇기.py
+++ Programmers/영어 끝말잇기.py
@@ -1,17 +1,29 @@
 # https://programmers.co.kr/learn/courses/30/lessons/12981
 
 def solution(n, words):
-    last=''
-    answer=[0,0]
-    done=[]
-    for i,w in enumerate(words):
-        if i==0:
-            last=w
+    last = ''
+    answer = [0, 0]
+    done = []
+    for i, w in enumerate(words):
+        if i == 0:
+            last = w
             done.append(w)
             continue
-        if last[-1]!=w[0] or w in done:
-            answer=[i%n+1,i//n+1]
+        if last[-1] != w[0] or w in done:
+            answer = [i % n + 1, i // n + 1]
             break
-        last=w
+        last = w
         done.append(w)
     return answer
+
+
+def solution_v2(n, words):
+    for i in range(1, len(words)):
+        for j in range(i):
+            if words[i] == words[j]:
+                break
+        else:
+            if words[i - 1][-1] == words[i][0]:
+                continue
+        return [i % n + 1, i // n + 1]
+    return [0, 0]
