56b90393f02ed86b4ade7e1541d26de18d03e910
maplejh <cjh0313c@gmail.com>
2022-06-08 23:23:13 +0900

    220608 프로그래머스 n^2배열 자르기

--- Programmers/n^2 배열 자르기.py
+++ Programmers/n^2 배열 자르기.py
@@ -2,6 +2,14 @@
 
 def solution(n, left, right):
     answer = []
-    for i in range(int(left), int(right) + 1): # int(left),int(right) 안하면 에러가 남
+    for i in range(int(left), int(right) + 1):  # int(left),int(right) 안하면 에러가 남
         answer.append(max(i // n, i % n) + 1)
+    return answer
+
+
+def solution_v2(n, left, right):
+    answer = []
+    for i in range(left, right + 1):
+        r, c = divmod(i, n)
+        answer.append(max(r, c) + 1)
     return answer
