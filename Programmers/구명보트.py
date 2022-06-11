7db56ffa61acc241e1656181011143fd6cff5e3a
maplejh <cjh0313c@gmail.com>
2022-06-11 22:08:43 +0900

    220611 프로그래머스 구명보트

--- Programmers/구명보트.py
+++ Programmers/구명보트.py
@@ -16,3 +16,19 @@ def solution(people, limit):
     if people:
         answer += 1
     return answer
+
+
+def solution_v2(people, limit):
+    answer = 0
+    people.sort()
+    start = 0
+    end = len(people) - 1
+    while start <= end:
+        if people[start] + people[end] <= limit:
+            start += 1
+            end -= 1
+            answer += 1
+        else:
+            end -= 1
+            answer += 1
+    return answer
