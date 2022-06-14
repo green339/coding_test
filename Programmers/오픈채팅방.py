0ef00fb120725ee600dbcba27cd99af431a7a43f
maplejh <cjh0313c@gmail.com>
2022-06-14 21:50:14 +0900

    220614 프로그래머스 오픈채팅방

--- Programmers/오픈채팅방.py
+++ Programmers/오픈채팅방.py
@@ -13,3 +13,21 @@ def solution(record):
         elif log[0][0] == "L":
             answer.append(uid[log[1]] + "님이 나갔습니다.")
     return answer
+
+
+from collections import defaultdict
+
+
+def solution_v2(record):
+    answer = []
+    mention = {'Enter': '님이 들어왔습니다.', 'Leave': '님이 나갔습니다.'}
+    name = defaultdict(list)
+    for r in record:
+        r = r.split()
+        if r[0][0] == 'E' or r[0][0] == 'C':
+            name[r[1]] = r[2]
+    for r in record:
+        r = r.split()
+        if r[0][0] != 'C':
+            answer.append(name[r[1]] + mention[r[0]])
+    return answer
