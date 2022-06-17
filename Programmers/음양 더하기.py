6b4a9ac92f7b4a57cdad6d0fc89fdbb1051a8a41
maplejh <cjh0313c@gmail.com>
2022-06-17 22:07:29 +0900

    220617 프로그래머스 음양 더하기

---
+++ Programmers/음양 더하기.py
@@ -0,0 +1,9 @@
+# https://programmers.co.kr/learn/courses/30/lessons/76501
+def solution(absolutes, signs):
+    answer = 0
+    for a, s in zip(absolutes, signs):
+        if s:
+            answer += a
+        else:
+            answer -= a
+    return answer
