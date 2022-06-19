18b3b11a0e1808a13488daecb4ea9c2422a1e2b2
maplejh <cjh0313c@gmail.com>
2022-06-19 21:43:52 +0900

    220619 프로그래머스 없는 숫자 더하기

--- Programmers/없는 숫자 더하기.py
+++ Programmers/없는 숫자 더하기.py
@@ -4,3 +4,7 @@ def solution(numbers):
     nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     answer = sum(set(nums) - set(numbers))
     return answer
+
+
+def solution_v2(numbers):
+    return 45 - sum(numbers)
