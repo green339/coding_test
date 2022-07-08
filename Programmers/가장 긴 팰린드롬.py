c7b0c1aed339473fff794b79ad7d15f8745f0070
maplejh <cjh0313c@gmail.com>
2022-07-08 23:17:30 +0900

    220708 프로그래머스 가장 긴 팰린드롬

---
+++ Programmers/가장 긴 팰린드롬.py
@@ -0,0 +1,14 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12904
+def solution(s):
+    answer = 0
+    n = len(s)
+    for i in range(n):  # 시작
+        for p in range(n - i, 0, -1):  # 팰린드롬 길이
+            if answer >= p:
+                break
+            for k in range(p // 2 + 1):
+                if s[i + k] != s[i + p - k - 1]:
+                    break
+            else:
+                answer = max(answer, p)
+    return answer
