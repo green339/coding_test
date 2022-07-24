67f142992c7ab8e6840d540f9e810818a2c88f1f
maplejh <cjh0313c@gmail.com>
2022-07-25 00:13:46 +0900

    220725 프로그래머스 예상 대진표

--- Programmers/예상 대진표.py
+++ Programmers/예상 대진표.py
@@ -12,5 +12,7 @@ def solution(n, a, b):
         a = math.ceil(a / 2)
         b = math.ceil(b / 2)
     return answer
-# 트리와 비트로 고려 (풀이참고)
-#    return ((a-1)^(b-1)).bit_length()
+
+
+def solution_v2(n,a,b):
+    return ((a-1)^(b-1)).bit_length()
