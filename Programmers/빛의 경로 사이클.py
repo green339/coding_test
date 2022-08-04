f65a2e2a0e3d157efbdd90e60db26c0884111ade
maplejh <cjh0313c@gmail.com>
2022-08-04 17:49:38 +0900

    220804 프로그래머스 빛의 경로 사이클

--- Programmers/빛의 경로 사이클.py
+++ Programmers/빛의 경로 사이클.py
@@ -35,3 +35,30 @@ def solution(grid):
                     visited[i][j][d] = 1
                     answer.append(cycle(i, j, d))
     return sorted(answer)
+
+
+## cycle에서 while문 종료조건 변경
+def solution(grid):
+    def cycle(x, y, d):
+        cost = 0
+        while not visited[x][y][d]:
+            visited[x][y][d] = 1
+            d = (d + turn[grid[x][y]]) % 4
+            x = (x + dx[d]) % r
+            y = (y + dy[d]) % c
+            cost += 1
+        return cost
+
+    dx = [0, 1, 0, -1]  # 동서남북
+    dy = [1, 0, -1, 0]
+    turn = {'L': -1, 'R': 1, 'S': 0}
+    answer = []
+    r = len(grid)
+    c = len(grid[0])
+    visited = [[[0, 0, 0, 0] for _ in range(c)] for _ in range(r)]
+    for i in range(r):
+        for j in range(c):
+            for d in range(4):
+                if not visited[i][j][d]:
+                    answer.append(cycle(i, j, d))
+    return sorted(answer)
