e56ce646b277a9226227ea557e24093b5a6fb769
maplejh <cjh0313c@gmail.com>
2022-08-13 23:18:07 +0900

    220813 프로그래머스 같은 숫자는 싫어

--- Programmers/같은 숫자는 싫어.py
+++ Programmers/같은 숫자는 싫어.py
@@ -1,3 +1,4 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12906
 from collections import deque
 
 
@@ -12,5 +13,12 @@ def solution(arr):
         else:
             tmp = x
             answer.append(x)
+    return answer
 
+
+def solution_v2(arr):
+    answer = [arr[0]]
+    for i in range(1, len(arr)):
+        if arr[i] != answer[-1]:
+            answer.append(arr[i])
     return answer
