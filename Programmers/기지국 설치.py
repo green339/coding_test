6531936e8ee875654af308b9af69ecd337eb4fa7
maplejh <cjh0313c@gmail.com>
2022-08-28 21:48:58 +0900

    220828 프로그래머스 기지국 설치

--- Programmers/기지국 설치.py
+++ Programmers/기지국 설치.py
@@ -4,7 +4,7 @@ from math import ceil
 
 def solution(n, stations, w):
     answer = 0
-    no_electric = [[1, n]] # 전파가 전달 X 부분
+    no_electric = [[1, n]]  # 전파가 전달 X 부분
     for s in stations:
         if no_electric:
             a, b = no_electric.pop()
@@ -16,4 +16,19 @@ def solution(n, stations, w):
             break
     for s, e in no_electric:
         answer += ceil((e - s + 1) / (2 * w + 1))
+    return answer
+
+
+def solution_v2(n, stations, w):
+    answer = 0
+    s = len(stations)
+    si = 0
+    pos = 1
+    while pos <= n:
+        if si < s and pos >= stations[si] - w:
+            pos = stations[si] + w + 1
+            si += 1
+        else:  # 기지국 설치
+            answer += 1
+            pos += 2 * w + 1
     return answer
