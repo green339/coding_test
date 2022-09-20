9fb9186eab4bced13f67b01dad49d4567264dd62
maplejh <cjh0313c@gmail.com>
2022-09-20 18:49:11 +0900

    220920 프로그래머스 합승 택시 요금

--- Programmers/합승 택시 요금.py
+++ Programmers/합승 택시 요금.py
@@ -10,7 +10,7 @@ def solution(n, s, a, b, fares):
     for w in range(1, n + 1):
         for x in range(1, n + 1):
             for y in range(1, n + 1):
-                if board[x][y]>board[x][w] + board[w][y]:
+                if board[x][y] > board[x][w] + board[w][y]:
                     board[x][y] = board[x][w] + board[w][y]
 
     answer = board[s][a] + board[s][b]
@@ -18,3 +18,35 @@ def solution(n, s, a, b, fares):
         answer = min(answer, board[s][i] + board[i][a] + board[i][b])
 
     return answer
+
+
+from collections import defaultdict
+import heapq
+
+
+def solution_v2(n, s, a, b, fares):
+    def distance(sx):
+        dist = [10e9] * (n + 1)
+        q = [(0, sx)]
+        dist[sx] = 0
+        while q:
+            cur, x = heapq.heappop(q)
+            if dist[x] < cur:
+                continue
+            for nx, cost in board[x]:
+                tmp = cost + cur
+                if tmp < dist[nx]:
+                    dist[nx] = tmp
+                    heapq.heappush(q, (tmp, nx))
+        return dist
+
+    board = defaultdict(list)
+    for c, d, f in fares:
+        board[c].append((d, f))
+        board[d].append((c, f))
+    distances = [0] * (n + 1)
+    for k in [s, a, b]:
+        tmp = distance(k)
+        for idx in range(n + 1):
+            distances[idx] += tmp[idx]
+    return min(distances)
