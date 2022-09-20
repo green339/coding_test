57151a8684de24c580cc036a7cd89dcfff0fc103
maplejh <cjh0313c@gmail.com>
2022-09-21 01:14:57 +0900

    220921 프로그래머스 동굴 탐험

--- Programmers/동굴 탐험.py
+++ Programmers/동굴 탐험.py
@@ -32,3 +32,41 @@ def solution(n, path, order):
     if min(visited) == 0:
         answer = False
     return answer
+
+
+def solution_v2(n, path, order):
+    # 진입차수 (0 빼고는 적어도 1)
+    indegree = [1] * n
+    indegree[0] = 0
+    board = defaultdict(list)
+    for a, b in path:
+        board[a].append(b)
+        board[b].append(a)
+    # 단방향 그래프 만들기
+    graph = defaultdict(list)
+    visited = [0] * n
+    q = deque()
+    q.append(0)
+    visited[0] = 1
+    while q:
+        x = q.popleft()
+        for nx in board[x]:
+            if not visited[nx]:
+                visited[nx] = 1
+                q.append(nx)
+                graph[x].append(nx)
+    for a, b in order:
+        graph[a].append(b)
+        indegree[b] += 1
+    # 위상정렬
+    q = deque()
+    q.append(0)
+    cnt = 0
+    while q:
+        cnt += 1
+        pre_node = q.popleft()
+        for post_node in graph[pre_node]:
+            indegree[post_node] -= 1
+            if not indegree[post_node]:
+                q.append(post_node)
+    return cnt == n
