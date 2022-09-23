1d4991219b63ec4f497e82f9f41b726232d60362
maplejh <cjh0313c@gmail.com>
2022-09-23 19:05:51 +0900

    220922 프로그래머스 코딩테스트 공부

--- Programmers/코딩 테스트 공부.py
+++ Programmers/코딩 테스트 공부.py
@@ -3,25 +3,25 @@ import heapq
 
 
 def solution(alp, cop, problems):
-    tmp = list(map(max, zip(*problems)))
-    max_alp = max(alp, tmp[0])
-    max_cop = max(cop, tmp[1])
-    # 시간을 1초씩 증가할 수 있도록
-    problems.append([0, 0, 0, 1, 1])
+    temp = list(map(max, zip(*problems)))
+    max_alp_req = max(alp, temp[0])
+    max_cop_req = max(cop, temp[1])
+    dist = [[30000] * (max_cop_req + 1) for _ in range(max_alp_req + 1)]
     problems.append([0, 0, 1, 0, 1])
-    dist = [[10e9] * (max_cop + 1) for _ in range(max_alp + 1)]  # (알고력, 코딩력) 까지의 거리
-    hq = [(0, alp, cop)]
-    while hq:
-        time, cur_alp, cur_cop = heapq.heappop(hq)
-        if dist[cur_alp][cur_cop] < time:
+    problems.append([0, 0, 0, 1, 1])
+    q = [(0, alp, cop)]
+    dist[alp][cop] = 0
+    while q:
+        cur_cost, cur_alp, cur_cop = heapq.heappop(q)
+        if dist[cur_alp][cur_cop] < cur_cost:
             continue
         for alp_req, cop_req, alp_rwd, cop_rwd, cost in problems:
             if alp_req <= cur_alp and cop_req <= cur_cop:
-                # 범위를 넘어도 문제는 풀 수 있음
-                next_alp = min(max_alp, cur_alp + alp_rwd)
-                next_cop = min(max_cop, cur_cop + cop_rwd)
-                tmp = time + cost
-                if dist[next_alp][next_cop] > tmp:
-                    dist[next_alp][next_cop] = tmp
-                    heapq.heappush(hq, (tmp, next_alp, next_cop))
+                # 범위를 넘어가는 경우도 고려
+                next_alp = min(max_alp_req, alp_rwd + cur_alp)
+                next_cop = min(max_cop_req, cop_rwd + cur_cop)
+                next_cost = cost + cur_cost
+                if dist[next_alp][next_cop] > next_cost:
+                    dist[next_alp][next_cop] = next_cost
+                    heapq.heappush(q, (next_cost, next_alp, next_cop))
     return dist[-1][-1]
