255f041b2a05d7b957b7bdf7666c3aa4c61ce9d8
maplejh <cjh0313c@gmail.com>
2022-10-29 21:11:18 +0900

    221029 프로그래머스 lv0

---
+++ Programmers/배열 회전시키기.py
@@ -0,0 +1,11 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120844
+from collections import deque
+
+
+def solution(numbers, direction):
+    answer = deque(numbers)
+    if direction == "right":
+        answer.rotate(1)
+    else:
+        answer.rotate(-1)
+    return list(answer)
