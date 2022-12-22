89dca71102895a0bb4dfed5442800cb780364240
maplejh <cjh0313c@gmail.com>
2022-12-23 00:51:19 +0900

    221223 프로그래머스 점 찍기

---
+++ Programmers/점 찍기.py
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/140107
+def solution(k, d):
+    answer = 0
+    for x in range(0,d+1,k):
+        y=int((d**2-x**2)**0.5)
+        answer+=y//k+1
+    return answer
